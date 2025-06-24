bb08c8f2a702e47092844cd3725b8bdf3f7ebf5c
Ely <christianjudevillaber@gmail.com>
2025-06-24 14:01:47 +0800

    Feat: Test  Player Lane Change

--- Assets/Scripts/Game/PlayerSystem/PlayerController.cs
+++ Assets/Scripts/Game/PlayerSystem/PlayerController.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 using Core.EventSystem;
-using Core.InputSystem;
 using Core.InputSystem.Events;
 
 namespace Game.PlayerSystem {
     public class PlayerController : MonoBehaviour {
+
+        // Lanes Testing
+        [Header("Lanes")]
+        [SerializeField] private int laneIndex = 2;
+        [SerializeField] private List<Transform> lanes = new List<Transform>();
+        [SerializeField] private float moveSpeed = 10f;
+
+        private Vector3 targetPos;
         private Rigidbody rb;
 
 
@@ -13,6 +21,15 @@ namespace Game.PlayerSystem {
 
         private void Awake() {
             rb = GetComponent<Rigidbody>();
+        }
+
+        private void Start() {
+            if (lanes.Count > 0 && laneIndex < lanes.Count) {
+                targetPos = new Vector3(lanes[laneIndex].position.x, transform.position.y, transform.position.z);
+            }
+            else {
+                Debug.LogError("Lanes not set up properly or invalid lane index!");
+            }
         }
 
         private void OnEnable() {
@@ -29,14 +46,31 @@ namespace Game.PlayerSystem {
             EventBus.Unsubscribe<Evt_OnSwipeDown>(OnSwipeDown);
         }
 
+        private void FixedUpdate() {
+            if (rb != null && Vector3.Distance(transform.position, targetPos) > 0.01f) {
+                Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.fixedDeltaTime);
+                rb.MovePosition(newPos);
+            }
+        }
+
 
         // ---------- Event Methods ----------
         private void OnSwipeLeft(Evt_OnSwipeLeft e) {
             Debug.Log("Swipe left");
+
+            if (laneIndex > 0) {
+                laneIndex--;
+                MoveToLane();
+            }
         }
 
         private void OnSwipeRight(Evt_OnSwipeRight e) {
             Debug.Log("Swipe right");
+
+            if (laneIndex < lanes.Count - 1) {
+                laneIndex++;
+                MoveToLane();
+            }
         }
 
         private void OnSwipeUp(Evt_OnSwipeUp e) {
@@ -45,6 +79,16 @@ namespace Game.PlayerSystem {
 
         private void OnSwipeDown(Evt_OnSwipeDown e) {
             Debug.Log("Swipe down");
+        }
+
+
+        // ---------- Methods ----------
+
+        private void MoveToLane() {
+            if (lanes.Count > 0 && laneIndex < lanes.Count) {
+                Vector3 newTargetPos = new Vector3(lanes[laneIndex].position.x, transform.position.y, transform.position.z);
+                targetPos = newTargetPos;
+            }
         }
     }
 }
